a796a34c9eeeea7ef6f89a0b67fae6ef6f239097
ITBill <704317542@qq.com>
2023-04-02 21:38:42 +0800

    update

--- Proj-Reversed-Reversi/bot_v_bot/MCTS/utils.py
+++ Proj-Reversed-Reversi/bot_v_bot/MCTS/utils.py
@@ -18,17 +18,20 @@ INIT_GRID = np.array([[0,  0,  0,  0,  0,  0,  0,  0],
                       [0,  0,  0,  0,  0,  0,  0,  0]])
 
 
+# def print_board(board):
+#     for row in range(BOARD_SIZE):
+#         line = []
+#         for col in range(BOARD_SIZE):
+#             stone = board[row, col]
+#             line.append(STONE_TO_CHAR[stone])
+#         print("%2s %s" % (row, ''.join(line)))
+    
+#     print("    " + "  ".join(str(e) for e in range(BOARD_SIZE)))
+
 def print_board(board):
     for row in range(BOARD_SIZE):
-        line = []
-        for col in range(BOARD_SIZE):
-            stone = board[row, col]
-            line.append(STONE_TO_CHAR[stone])
-        print("%2s %s" % (row, ''.join(line)))
-    
+        print("%2s %s" % (row, ''.join([STONE_TO_CHAR[board[row, col]] for col in range(BOARD_SIZE)])))
     print("    " + "  ".join(str(e) for e in range(BOARD_SIZE)))
-
-
 
 def cost_time(func):
     def fun(*args, **kwargs):
